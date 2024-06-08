22ed13272c7a7e1c7198b55b15e5350ddc7eec21
Rukky <abrukevwe@gmail.com>
2024-06-08 03:16:34 +0100

    Firebase setup

--- api.js
+++ api.js
@@ -1,3 +1,22 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBP9YIuTEYx0ax06GsYEsY9AAZ2aqDLR2I",
+  authDomain: "vanlife-68ec1.firebaseapp.com",
+  projectId: "vanlife-68ec1",
+  storageBucket: "vanlife-68ec1.appspot.com",
+  messagingSenderId: "335838025109",
+  appId: "1:335838025109:web:1ed391d67f3a09a7ee529c"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+
+
 export async function getVans(id) {
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
